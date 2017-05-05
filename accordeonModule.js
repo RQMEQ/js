7983f11f7dc555cfeb467aa4a1b16e175e7cd68a
RQMEQ <roman_g86@mail.ru>
2017-05-05 10:50:06 +0300

    Ready akk

--- accordeonModule.js
+++ accordeonModule.js
@@ -2,38 +2,17 @@
 
 var accordeonModule = function (data) { 
 
-	//var panelItem = document.querySelectorAll('.box-default'),
-	//active = document.getElementsByClassName('active');
 	
-
-	/*Array.from(panelItem).forEach(function(item, i, panelItem) {
-		item.addEventListener('click', function(e) {
-    if (active.length > 0 && active[0] !== this) // если есть активный элемент, и это не тот по которому кликнули
-      active[0].classList.remove('collapsed-box'); // убрать класс collapsed-box
-    // изменить состояние класса panel-active на текущем элементе: добавить если не было, убрать если было.
-    this.classList.toggle('collapsed-box');
-});S
-	});*/
-	/*if (classes.contains('fa-plus')) {
-
-			this.classList.remove('fa-plus');
-			this.classList.add('fa-minus');
-		}*/
 		
 	
 		$h.d('.accordeon-Module', 'click', '.btn-box-tool', function(e) {
 			
 			var openElem = e.target.closest('.box-default');
 			
-			//if(this.classes.contains('fa-plus')){
-				//this.classList.remove('fa-minus');
-			//}else {
-				//this.classList.add('fa-plus');
-			//}
+			
 		if (openElem.classList.contains('collapsed-box') === true) {
 			openElem.classList.remove('collapsed-box');
-			//this.children.classList.remove('fa-plus');
-			//this.children.classList.add('fa-minus');
+			
 			this.children[0].classList.remove('fa-plus');
 			this.children[0].classList.add('fa-minus');
 		}
@@ -47,30 +26,12 @@ var accordeonModule = function (data) {
 	
   
 		
-		/*if ('.box-default:not(.collapsed-box)') {
-			alert(openElem.classList.contains('myCssClass'))
-		}*/
+		
 		
 		
 	});
 
-	/*var div = $('.collapsed-box');
-        div.click(function(){
-             div.not($(this).toggleClass('collapsed-box')).removeClass('collapsed-box')
-        });*/
-
-
-	/*classList.add
-classList.remove
-classList.toggle
-classList.contains отдасть булевое значение*/
-/*li:not(.elem)*/
 	
-	/*$(".button").click(function(){
-		
-		$(".element").toggleClass("collapsed-box");
-		
-	});*/
 
 	
 
